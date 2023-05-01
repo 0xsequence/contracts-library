58fbb807ef026f8b0126451bcd416f37c84269fb
Michael Standen <screaminghawk@gmail.com>
2023-05-02 10:32:18 +1200

    Add ERC1155Sale base implementation

---
+++ src/tokens/ERC1155/ERC1155SaleErrors.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.17;
+
+// A contract for errors for extensibility.
+contract ERC1155SaleErrors {
+    /**
+     * Sale is not active.
+     * @param tokenId Invalid Token ID.
+     */
+    error SaleInactive(uint256 tokenId);
+
+    /**
+     * Insufficient tokens for payment.
+     * @param expected Expected amount of tokens.
+     * @param actual Actual amount of tokens.
+     */
+    error InsufficientPayment(uint256 expected, uint256 actual);
+
+    /**
+     * Withdraw failed.
+     */
+    error WithdrawFailed();
+}
