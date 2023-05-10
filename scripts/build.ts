1f54f2d336c331ebf4a834f4ac29d802459bfaca
Michael Standen <screaminghawk@gmail.com>
2023-05-11 07:50:06 +1200

    Add build script

---
+++ scripts/build.ts
@@ -0,0 +1,55 @@
+import { exec as execNonPromise } from 'child_process'
+import { copyFile, mkdir, readFile, rmdir, writeFile } from 'fs/promises'
+import { join } from 'path'
+import util from 'util'
+import { BUILD_DIR, DEPLOYABLE_CONTRACT_NAMES } from './constants'
+const exec = util.promisify(execNonPromise)
+
+const main = async () => {
+  // Clean
+  await rmdir(BUILD_DIR, { recursive: true })
+
+  // Build with forge
+  console.log('Building contracts')
+  await exec('forge build')
+  console.log('Contracts built')
+
+  await mkdir(BUILD_DIR, { recursive: true })
+
+  // Create the compiler input files
+  for (const solFile of DEPLOYABLE_CONTRACT_NAMES) {
+    const forgeOutputDir = `out/${solFile}.sol`
+    const compilerDetails = JSON.parse(
+      await readFile(join(forgeOutputDir, `${solFile}.metadata.json`), 'utf8'),
+    )
+
+    // Replace source urls with file contents
+    for (const sourceKey of Object.keys(compilerDetails.sources)) {
+      compilerDetails.sources[sourceKey].contents = await readFile(
+        join(sourceKey),
+        'utf8',
+      )
+    }
+
+    // Write the compiler input file
+    await writeFile(
+      join(BUILD_DIR, `${solFile}.input.json`),
+      JSON.stringify(compilerDetails),
+    )
+
+    // Copy the compiler output too
+    await copyFile(
+      `${forgeOutputDir}/${solFile}.json`,
+      `${BUILD_DIR}/${solFile}.json`,
+    )
+  }
+}
+
+main()
+  .then(() => {
+    console.log('Done')
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
