471b5dabe9793c3c452ba3e266dcaef71ea9d596
Michael Standen <mstan@horizon.io>
2024-09-10 14:13:24 +1200

    ERC721C

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -2,6 +2,7 @@ export const BUILD_DIR = 'build'
 export const DEPLOYABLE_CONTRACT_NAMES = [
   'ERC20ItemsFactory',
   'ERC721ItemsFactory',
+  'ERC721CItemsFactory',
   'ERC721SaleFactory',
   'ERC1155ItemsFactory',
   'ERC1155SaleFactory',
@@ -10,6 +11,7 @@ export const DEPLOYABLE_CONTRACT_NAMES = [
 export const TOKEN_CONTRACT_NAMES = [
   'ERC20Items',
   'ERC721Items',
+  'ERC721CItems',
   'ERC721Sale',
   'ERC1155Items',
   'ERC1155Sale',
