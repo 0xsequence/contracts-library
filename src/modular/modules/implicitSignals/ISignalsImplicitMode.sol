0a732781bbb5edc88db61fc43a6da53072096147
Michael Standen <mstan@horizon.io>
2025-08-07 11:00:54 +1200

    ProxyFactory supports implicit signals

--- src/modular/modules/implicitSignals/ISignalsImplicitMode.sol
+++ src/modular/modules/implicitSignals/ISignalsImplicitMode.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.19;
 
-import { ISignalsImplicitMode } from "lib/signals-implicit-mode/src/helper/SignalsImplicitMode.sol";
+//solhint-disable-next-line no-unused-import
+import {
+    Attestation, ISignalsImplicitMode, Payload
+} from "lib/signals-implicit-mode/src/helper/SignalsImplicitMode.sol";
--- src/modular/modules/modularProxy/ModularProxyFactory.sol
+++ src/modular/modules/modularProxy/ModularProxyFactory.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.19;
 
+import { ISignalsImplicitMode, Payload } from "../implicitSignals/ISignalsImplicitMode.sol";
 import { IModularProxyFactory } from "./IModularProxyFactory.sol";
 import { ModularProxy } from "./ModularProxy.sol";
 import { Create2 } from "lib/openzeppelin-contracts/contracts/utils/Create2.sol";
+import {
+    Attestation,
+    LibAttestation
+} from "lib/signals-implicit-mode/lib/sequence-v3/src/extensions/sessions/implicit/Attestation.sol";
 
 /// @title ModularProxyFactory
 /// @author Michael Standen
 /// @notice Factory for creating modular proxy instances.
-contract ModularProxyFactory is IModularProxyFactory {
+contract ModularProxyFactory is IModularProxyFactory, ISignalsImplicitMode {
+
+    using LibAttestation for Attestation;
 
     /// @inheritdoc IModularProxyFactory
     function deploy(uint256 nonce, address defaultImpl, address owner) external returns (ModularProxy) {
@@ -33,6 +40,16 @@ contract ModularProxyFactory is IModularProxyFactory {
         salt = keccak256(abi.encode(nonce, defaultImpl, owner));
         creationCode = abi.encodePacked(type(ModularProxy).creationCode, abi.encode(defaultImpl, owner));
         return (salt, creationCode);
+    }
+
+    /// @inheritdoc ISignalsImplicitMode
+    function acceptImplicitRequest(
+        address wallet,
+        Attestation calldata attestation,
+        Payload.Call calldata
+    ) external pure returns (bytes32 magic) {
+        // Allow implicit access for any domain
+        return attestation.generateImplicitRequestMagic(wallet);
     }
 
 }
